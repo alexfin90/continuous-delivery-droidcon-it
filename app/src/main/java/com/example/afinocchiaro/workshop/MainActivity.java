f324128b21e62711d4448eb92b8cc1038bbdb45a
Finocchiaro, Alessandro (IT - Milano) <afinocchiaro@deloitte.it>
2017-04-06 14:54:35 +0200

    fabric integration

--- app/src/main/java/com/example/afinocchiaro/workshop/MainActivity.java
+++ app/src/main/java/com/example/afinocchiaro/workshop/MainActivity.java
@@ -2,10 +2,13 @@ package com.example.afinocchiaro.workshop;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import com.crashlytics.android.Crashlytics;
+import io.fabric.sdk.android.Fabric;
 public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Fabric.with(this, new Crashlytics());
         setContentView(R.layout.activity_main);
         Log.i("Ciao","Ciao");
     }
